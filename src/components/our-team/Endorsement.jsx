023106fd18490c51d6c22a51f3380f1bc9d75241
stefanolami <stefanolami90@gmail.com>
2024-08-29 19:03:16 +0300

    feat: transform content in team.js in arrays, add spacing in teamMember paragraph

--- src/components/our-team/Endorsement.jsx
+++ src/components/our-team/Endorsement.jsx
@@ -1,14 +1,21 @@
 export default function Endorsement({ endorsement }) {
 	return (
-		<div className="w-screen -ml-[5vw] mb-8 bg-primary-light p-6 flex flex-col justify-between items-center overflow text-white">
-			<span className="font-jose text-xs italic text-justify mb-4">
-				{endorsement.content}
-			</span>
+		<div className="w-screen xl:w-full -ml-[5vw] xl:ml-0 mb-8 xl:mb-20 bg-primary-light p-6 xl:px-16 xl:py-8 flex flex-col justify-between items-center overflow text-white">
+			<div className="font-jose text-xs italic xl:text-xl text-justify mb-4">
+				{endorsement.content.map((element, index) => (
+					<p
+						className="mb-4 xl:mb-6"
+						key={index}
+					>
+						{element}
+					</p>
+				))}
+			</div>
 			<div className="flex flex-col items-end w-full">
-				<span className="font-jose text-xs italic font-bold text-right block">
+				<span className="font-jose text-xs xl:text-xl italic font-bold text-right block">
 					{endorsement.name}
 				</span>
-				<span className="font-jose text-xs italic text-right block">
+				<span className="font-jose text-xs xl:text-xl italic text-right block">
 					{endorsement.title}
 				</span>
 			</div>
--- src/components/our-team/TeamMember.jsx
+++ src/components/our-team/TeamMember.jsx
@@ -6,7 +6,7 @@ export default function TeamMember({ name }) {
 	console.log(member)
 
 	return (
-		<div className="w-[90%] xl:w-[75%] mx-auto">
+		<div className="w-[90%] xl:w-[70%] mx-auto">
 			<div className="w-full flex flex-row items-start justify-between mb-8">
 				<div className="mt-4 xl:mt-8">
 					<h2 className="font-unna font-bold text-base xl:text-[48px] mb-4 xl:mb-8 uppercase">
@@ -20,17 +20,31 @@ export default function TeamMember({ name }) {
 				</div>
 				<div className="bg-primary-light w-1/4 aspect-[6/9]"></div>
 			</div>
-			<div className="font-jose text-xs text-justify mb-8">
-				{member.introduction}
+			<div className="font-jose text-xs xl:text-lg text-justify mb-2 xl:mb-6">
+				{member.introduction.map((element, index) => (
+					<p
+						className="mb-4 xl:mb-6"
+						key={index}
+					>
+						{element}
+					</p>
+				))}
 			</div>
 			{member.paragraphs.map((paragraph, index) => (
 				<div key={index}>
-					<h3 className="font-unna font-bold text-xl mb-4">
+					<h3 className="font-unna font-bold text-xl xl:text-3xl mb-4 xl:mb-6">
 						{paragraph.title}
 					</h3>
-					<p className="font-jose text-xs mb-4 text-justify">
-						{paragraph.content}
-					</p>
+					<div className="font-jose text-xs xl:text-lg mb-2 xl:mb-4 text-justify">
+						{paragraph.content.map((element, index) => (
+							<p
+								className="mb-4 xl:mb-6"
+								key={index}
+							>
+								{element}
+							</p>
+						))}
+					</div>
 					{paragraph.endorsement && (
 						<div>
 							<Endorsement endorsement={paragraph.endorsement} />
@@ -38,6 +52,19 @@ export default function TeamMember({ name }) {
 					)}
 				</div>
 			))}
+			<div className="mt-10">
+				<h3 className="font-unna font-bold text-xl xl:text-3xl xl:mb-2">
+					Contact
+				</h3>
+				<span className="w-full h-[2px] bg-black block"></span>
+				<ul className="font-jose text-xs xl:text-lg mt-2">
+					{member.contact.phone && <li>{member.contact.phone}</li>}
+					{member.contact.email && <li>{member.contact.email}</li>}
+					{member.contact.address && (
+						<li>{member.contact.address}</li>
+					)}
+				</ul>
+			</div>
 		</div>
 	)
 }
