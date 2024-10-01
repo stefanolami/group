9dbc7f918f67f3a9d8701a0500a9ff7f452a61d2
stefanolami <stefanolami90@gmail.com>
2024-10-01 09:53:44 +0300

    feat: add pillars and products data and new icons

--- src/data/data.js
+++ src/data/data.js
@@ -2,50 +2,71 @@ export const pillars = [
 	{
 		id: 'consulting',
 		icon: '/mono/pillars/consulting-mono.png',
+		grid: '/logos/grid/pillars/consulting',
 		primary: '#354B83',
 		secondary: '#FFFFFF',
 		tertiary: '#FFFFFF',
-		text: 'Time&Place Consulting is a strategic communications, Public Affairs and project management agency which brings together a global team of experts in government relations, legal affairs, association management, public funding as well as reputation and crisis management.',
+		text: [
+			'Strategic communications and public affairs agency which brings together a global team of experts in government relations, legal affairs, business management, association management, as well as reputation and crisis management. Our goal is to create healthy market, regulatory and public perception contexts for your ideas, products and services, no matter if innovative or tradition.',
+		],
 	},
 	{
 		id: 'funding',
 		icon: '/pillars/funding.png',
+		grid: '/logos/grid/pillars/funding',
 		primary: '#FFFFFF',
 		secondary: '#00A4D5',
 		tertiary: '#1D4967',
-		text: 'An European-wide agency which provides access to EU, national, regional and local public funding schemes for big business, SMEs, NGOs and local administration: from identifying opportunities to coordinating the project and running the consortium.',
+		text: [
+			'Home to the collective knowledge of Time&Place Group, where education, training and think tank capacities are offered.',
+			'With a cross-discipline approach we provide tailored courses, but also help decision-making have the necessary bedrock, no matter if industry, civil society or government.',
+		],
 	},
 	{
 		id: 'marketing',
 		icon: '/mono/pillars/marketing-mono.png',
+		grid: '/logos/grid/pillars/marketing',
 		primary: '#AA337F',
 		secondary: '#FFFFFF',
 		tertiary: '#FFFFFF',
-		text: 'A marketing agency specializing in brand strategy, communication and marketing solutions. From crafting compelling brand narratives to implementing targeted social media or digital campaigns, we provide comprehensive marketing services tailored to meet diverse business needs.',
+		text: [
+			'A marketing agency specializes in brand strategy, communication, and customized marketing solutions. We craft compelling brand narratives, implement targeted social media and digital campaigns, and offer comprehensive services tailored to diverse business needs. From developing a unique brand identity to creating marketing materials and building websites, we ensure your brand resonates with your audience and drives results.',
+		],
 	},
 	{
 		id: 'production',
 		icon: '/pillars/production.png',
+		grid: '/logos/grid/pillars/production',
 		primary: '#FFFFFF',
 		secondary: '#DF8946',
 		tertiary: '#4A2C2O',
-		text: 'An audio-visual media production company which produce commercials, movies, documentaries, series and music videos, but also profiles organisations, people and events.',
+		text: [
+			'The media branch of Time&Place Group with a spectrum of audiovisual products which can be adapted to your needs, no matter if you want to create profiling videos of your association, podcasts, or even documentaries.',
+			'Be ready! We will soon be diving into the worlds of live performance and film production.',
+		],
 	},
 	{
 		id: 'institute',
 		icon: '/mono/pillars/institute-mono.png',
+		grid: '/logos/grid/pillars/institute',
 		primary: '#6D9544',
 		secondary: '#FFFFFF',
 		tertiary: '#FFFFFF',
-		text: 'The Institute’s mission is to combine the knowledge pillars towards creating cross-discipline capacities for our clients. We take it further for training and education, but also for helping decision-making have the necessary bedrock, no matter if industry, civil society or government.',
+		text: [
+			'Home to the collective knowledge of Time&Place Group, where education, training and think tank capacities are offered.',
+			'With a cross-discipline approach we provide tailored courses, but also help decision-making have the necessary bedrock, no matter if industry, civil society or government.',
+		],
 	},
 	{
 		id: 'spaces',
 		icon: '/grid/spaces.png',
+		grid: '/logos/grid/pillars/spaces',
 		primary: '#FFFFFF',
 		secondary: '#0F2CCA',
 		tertiary: '#2A2A2A',
-		text: 'Coworking spaces, dedicated to provide unparalleled solutions tailored to our clients’ diverse needs. Our versatile facilities cater to provide the perfect setting for corporate events, networking mixers, workshops, and flexible office solutions tailored to individual preferences.',
+		text: [
+			'Your 360°-cradle-to-cradle event, meeting and office space management service. We provide the right spaces, and develop event concepts whilst making sure every aspect from the agenda to the take-aways reflect the messages your organization wants to promote. Our ultimate aims: A perfect fit and memorable.',
+		],
 	},
 ]
 
@@ -53,49 +74,94 @@ export const products = [
 	{
 		id: 'ecity',
 		icon: '/logos/products/ecity.png',
+		grid: '/logos/grid/products/ecity',
 		background: '#EAEAEA',
 		primary: '#012DD3',
 		text: [
-			"E-City is not just an e-mobility hub; it's a dynamic ecosystem designed to catalyze innovation, collaboration, and sustainable growth.",
-			"At E-City, we're revolutionizing the way cities connect through e-mobility. Our hub serves as a dynamic environment where individuals, businesses, and communities come together to embrace sustainable transportation solutions and foster innovation.",
-			'E-City is a vibrant community space. We function within office spaces which provide meeting areas, shared and private offices and collaborative workspaces, accessible to you, where like-minded individuals can gather, exchange ideas, and form partnerships.',
-			'E-City also hosts comprehensive training programs and educational resources for individuals and businesses alike developed by industry experts. E-City is built around a community of professionals that serve the whole ecosystem for sustainable electrification and sustainable transportation system.',
+			'A 360°-strategy development and implementation consultancy and think tank, focusing on the ongoing transition to electrification. Our building blocks are (1) electrified transport, agriculture, construction and mining; (2) energy management with a view of coordinating energy usage; (3) efficiency architecture through integrated ecosystems; (4) cost-effectiveness & efficiency based on a viable business plan.',
 		],
 	},
 	{
-		id: 'ecity',
-		icon: '/logos/products/ecity.png',
-		background: '#FFFFFF',
-		primary: '#012DD3',
-		text: [
-			"E-City is not just an e-mobility hub; it's a dynamic ecosystem designed to catalyze innovation, collaboration, and sustainable growth.",
-			"At E-City, we're revolutionizing the way cities connect through e-mobility. Our hub serves as a dynamic environment where individuals, businesses, and communities come together to embrace sustainable transportation solutions and foster innovation.",
-			'E-City is a vibrant community space. We function within office spaces which provide meeting areas, shared and private offices and collaborative workspaces, accessible to you, where like-minded individuals can gather, exchange ideas, and form partnerships.',
-			'E-City also hosts comprehensive training programs and educational resources for individuals and businesses alike developed by industry experts. E-City is built around a community of professionals that serve the whole ecosystem for sustainable electrification and sustainable transportation system.',
-		],
-	},
-	{
-		id: 'ecity',
-		icon: '/logos/products/ecity.png',
+		id: 'hubble',
+		icon: '/logos/grid/products/hubble.png',
+		grid: '/logos/grid/products/hubble',
 		background: '#EAEAEA',
 		primary: '#012DD3',
 		text: [
-			"E-City is not just an e-mobility hub; it's a dynamic ecosystem designed to catalyze innovation, collaboration, and sustainable growth.",
-			"At E-City, we're revolutionizing the way cities connect through e-mobility. Our hub serves as a dynamic environment where individuals, businesses, and communities come together to embrace sustainable transportation solutions and foster innovation.",
-			'E-City is a vibrant community space. We function within office spaces which provide meeting areas, shared and private offices and collaborative workspaces, accessible to you, where like-minded individuals can gather, exchange ideas, and form partnerships.',
-			'E-City also hosts comprehensive training programs and educational resources for individuals and businesses alike developed by industry experts. E-City is built around a community of professionals that serve the whole ecosystem for sustainable electrification and sustainable transportation system.',
+			'A permanent platform for the disability and down syndrome sports, music, culture and media communities. Our mission is to sustain and increase momentum and support of the disability and down syndrome movements in the decision-making processes at political, social and business levels. Our ideal POTEST world? Where decision-making processes understand the realities regarding the differences between those with disabilities and/or down syndrome, and those without, whilst at the same time not differentiating in creating accessibility and opportunities for either.',
 		],
 	},
 	{
-		id: 'ecity',
-		icon: '/logos/products/ecity.png',
-		background: '#FFFFFF',
+		id: 'poe',
+		icon: '/logos/grid/products/poe.png',
+		grid: '/logos/grid/products/poe',
+		background: '#EAEAEA',
 		primary: '#012DD3',
 		text: [
-			"E-City is not just an e-mobility hub; it's a dynamic ecosystem designed to catalyze innovation, collaboration, and sustainable growth.",
-			"At E-City, we're revolutionizing the way cities connect through e-mobility. Our hub serves as a dynamic environment where individuals, businesses, and communities come together to embrace sustainable transportation solutions and foster innovation.",
-			'E-City is a vibrant community space. We function within office spaces which provide meeting areas, shared and private offices and collaborative workspaces, accessible to you, where like-minded individuals can gather, exchange ideas, and form partnerships.',
-			'E-City also hosts comprehensive training programs and educational resources for individuals and businesses alike developed by industry experts. E-City is built around a community of professionals that serve the whole ecosystem for sustainable electrification and sustainable transportation system.',
+			'A permanent platform for the disability and down syndrome sports, music, culture and media communities. Our mission is to sustain and increase momentum and support of the disability and down syndrome movements in the decision-making processes at political, social and business levels. Our ideal POTEST world? Where decision-making processes understand the realities regarding the differences between those with disabilities and/or down syndrome, and those without, whilst at the same time not differentiating in creating accessibility and opportunities for either.',
+		],
+	},
+	{
+		id: 'potest',
+		icon: '/logos/grid/products/potest.png',
+		grid: '/logos/grid/products/potest',
+		background: '#EAEAEA',
+		primary: '#012DD3',
+		text: [
+			'A permanent platform for the disability and down syndrome sports, music, culture and media communities. Our mission is to sustain and increase momentum and support of the disability and down syndrome movements in the decision-making processes at political, social and business levels. Our ideal POTEST world? Where decision-making processes understand the realities regarding the differences between those with disabilities and/or down syndrome, and those without, whilst at the same time not differentiating in creating accessibility and opportunities for either.',
+		],
+	},
+	{
+		id: 'ckc',
+		icon: '/logos/grid/products/ckc.png',
+		grid: '/logos/grid/products/ckc',
+		background: '#EAEAEA',
+		primary: '#012DD3',
+		text: [
+			'A think tank which builds a universal understanding of the opportunities and challenges which digital and crypto currencies bring so that all relevant stakeholders including the public, law-makers, regulatory authorities, actual and potential users make the most informed decisions. We bring together industry, financial compliance experts, enforcement bodies, governmental decision-makers, and civil society organisations to provide the most complete view of what the technology does and what impact it has.',
+		],
+	},
+	{
+		id: 'teddy',
+		icon: '/logos/grid/products/teddy.png',
+		grid: '/logos/grid/products/teddy',
+		background: '#EAEAEA',
+		primary: '#012DD3',
+		text: [
+			'A 360°-strategy development and implementation consultancy and think tank, focusing on the ongoing transition to electrification. Our building blocks are (1) electrified transport, agriculture, construction and mining; (2) energy management with a view of coordinating energy usage; (3) efficiency architecture through integrated ecosystems; (4) cost-effectiveness & efficiency based on a viable business plan.',
+		],
+	},
+]
+
+export const projects = [
+	{
+		id: 'mozart',
+		icon: '/logos/grid/projects/mozart.png',
+		grid: '/logos/grid/projects/mozart',
+		background: '#EAEAEA',
+		primary: '#012DD3',
+		text: [
+			'Waterloo International Chamber Music Festival switching between Waterloo (Belgium) to Nagakute (Japan) from year to year. ',
+		],
+	},
+	{
+		id: 'ai',
+		icon: '/logos/grid/projects/ai.png',
+		grid: '/logos/grid/projects/ai',
+		background: '#EAEAEA',
+		primary: '#012DD3',
+		text: [
+			'A (for now) temporary constellation that focuses on addressing the following questions: (1) What tools can we apply at Time&Place to help with our internal infrastructure and for servicing clients? (2) What can we offer clients as a service for them to integrate AI? Powered by humans, and a bit of AI. ',
+		],
+	},
+	{
+		id: 'bin',
+		icon: '/logos/grid/projects/bin.png',
+		grid: '/logos/grid/projects/bin',
+		background: '#EAEAEA',
+		primary: '#012DD3',
+		text: [
+			'BIN@ is an informal international network of academic and industry partners engaged in supporting the creation of a sustainable forum for sharing good practices and opportunities in innovation. In 2025, Brussels with the support of Time&Place Group will host the two-day mid-term event.',
 		],
 	},
 ]
