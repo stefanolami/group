4809ed3eb584fe419c8af8ea3be6edc6a533c9b9
stefanolami <stefanolami90@gmail.com>
2024-10-03 20:31:02 +0300

    style: change favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
 import './globals.css'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'T&P Group',
+	description: 'Time&Place Group',
+}
 
 export default function RootLayout({
 	children,
