1bfe57c132c49f85d7c7950319dfaefd35b6b57c
stefanolami <stefanolami90@gmail.com>
2024-10-05 16:33:47 +0300

    Refactor: Update CookieBanner component styling and links

--- src/components/CookieBanner.jsx
+++ src/components/CookieBanner.jsx
@@ -31,33 +31,35 @@ export default function CookieBanner() {
 	}
 
 	return (
-		<div className="fixed bottom-3 left-3 right-3 bg-white text-black p-4 rounded-md shadow-md z-40">
-			<div className="container mx-auto flex justify-between items-center font-jose text-primary xl:text-lg">
+		<div className="fixed bottom-3 left-3 right-3 bg-white text-black p-4 rounded-md shadow-xl z-40">
+			<div className="container mx-auto flex justify-between items-center font-jose text-primary text-sm xl:text-lg">
 				<p>
 					We use cookies to ensure you get the best experience on our
 					website. Learn more here:
-					<Link
-						href="/cookie-use"
-						className="underline mx-3"
-					>
-						Cookie Use
-					</Link>
-					<Link
-						href="/privacy-policy"
-						className="underline"
-					>
-						Privacy Policy
-					</Link>
+					<div className="block xl:inline">
+						<Link
+							href="/cookie-use"
+							className="underline xl:ml-3 mr-3"
+						>
+							Cookie Use
+						</Link>
+						<Link
+							href="/privacy-policy"
+							className="underline"
+						>
+							Privacy Policy
+						</Link>
+					</div>
 				</p>
-				<div>
+				<div className="grid grid-rows-2 gap-2 xl:grid-cols-2 xl:grid-rows-1">
 					<button
-						className="bg-primary-light shadow-md hover:shadow-xl text-white px-4 py-2 rounded mr-4 hover:scale-105"
+						className="bg-primary-light shadow-md hover:shadow-xl text-white w-20 h-8 xl:w-24 xl:h-11 rounded hover:scale-105"
 						onClick={() => setCookieConsent(true)}
 					>
 						Accept
 					</button>
 					<button
-						className="bg-primary shadow-md hover:shadow-xl text-white px-4 py-2 rounded hover:scale-105"
+						className="bg-primary shadow-md hover:shadow-xl text-white w-20 h-8 xl:w-24 xl:h-11 rounded hover:scale-105"
 						onClick={() => setCookieConsent(false)}
 					>
 						Decline
