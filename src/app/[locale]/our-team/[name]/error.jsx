da3be5e47b7bb97e937d903a0640c37562a18417
stefanolami <stefanolami90@gmail.com>
2024-10-07 18:10:11 +0300

    refactor: Update error component styles in [locale] and [locale]/our-team/[name] directories

--- src/app/[locale]/our-team/[name]/error.jsx
+++ src/app/[locale]/our-team/[name]/error.jsx
@@ -2,7 +2,7 @@
 
 export default function ErrorBoundary({ error }) {
 	return (
-		<div className="w-full h-screen flex-col gap-5  text-2xl font-jose flex items-center justify-center bg-primary text-white -mt-16 xl:-mt-24">
+		<div className="px-[10%] h-screen flex-col gap-5 text-lg xl:text-2xl font-jose flex items-center justify-center text-center bg-primary text-white -mt-16 xl:-mt-24">
 			We&apos;re sorry, this team member doesn&apos;t seem to exist. Make
 			sure the name is correct and try again.
 		</div>
