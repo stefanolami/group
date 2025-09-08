9329a524e57d72ea1eba2f3684d7de7012b3d152
stefanolami <stefanolami90@gmail.com>
2025-09-08 16:32:15 +0300

    fix: adjust header layout and styling for improved responsiveness

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -58,30 +58,32 @@ export default function Header(messages) {
 			animate={hidden ? 'hidden' : 'visible'}
 			transition={{ duration: 0.3, ease: 'easeInOut' }}
 			className={
-				'fixed top-0 w-full text-white z-50 px-6 md:px-10 xl:px-[7%] flex justify-between items-center h-16 xl:h-24'
+				'fixed top-0 w-full bg-transparent text-white z-50 px-6 md:px-10 xl:px-[7%]'
 			}
 		>
-			<div className="flex items-center justify-center w-auto xl:w-[270px] h-full">
-				<Link
-					className="pl-6 md:pl-0 md:py-4 xl:py-7 w-[124px] xl:w-[231px] aspect-[694/186] relative xl:scale-75"
-					href="/"
-				>
-					<Image
-						src={icon}
-						alt="T&P Logo"
-						fill
-						sizes="(max-width: 640px) 40vw, 40vw"
-						quality={100}
-					/>
-				</Link>
+			<div className="flex justify-between items-center max-w-[1300px] mx-auto h-16 xl:h-24">
+				<div className="flex items-center justify-center h-full md:pl-4 lg:pl-0">
+					<Link
+						className="pl-6 md:pl-0 md:py-4 xl:py-7 w-[124px] xl:w-[231px] aspect-[694/186] relative xl:scale-75"
+						href="/"
+					>
+						<Image
+							src={icon}
+							alt="T&P Logo"
+							fill
+							sizes="(max-width: 640px) 40vw, 40vw"
+							quality={100}
+						/>
+					</Link>
+				</div>
+
+				<DesktopNav
+					inverted={inverted}
+					messages={navTrans}
+				/>
+
+				<MobileNav messages={navTrans} />
 			</div>
-
-			<DesktopNav
-				inverted={inverted}
-				messages={navTrans}
-			/>
-
-			<MobileNav messages={navTrans} />
 		</motion.div>
 	)
 }
